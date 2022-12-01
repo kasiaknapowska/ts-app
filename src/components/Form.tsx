8b407c699a72d0fc72b8f81685a6a05a6b81efa3
kasiaknapowska <k.knapowska@gmail.com>
2022-12-01 18:47:01 +0100

    form styles

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 export default function Form({}: Props) {
   return (
-   <div className='form_container'>
-   <h1>Formularz rejestracyjny</h1>
-    <form>
-          <label>
-              Login:
-              <input />
-            </label>
-    </form>
-   </div>
-  )
+    <div className="form_container">
+      <h1>Formularz rejestracyjny</h1>
+      <form>
+        <label>
+          Login:
+          <input />
+        </label>
+        <label>
+          Hasło:
+          <input />
+        </label>
+        <label>
+          Email:
+          <input />
+        </label>
+        <label>
+          Numer telefonu:
+          <input />
+        </label>
+        <label className="checkbox_label"><input className="checkbox" type="checkbox" /> Akceptuję regulamin</label>
+        <button className="btn btn_submit" type="submit">zapisz</button>
+      </form>
+    </div>
+  );
 }
