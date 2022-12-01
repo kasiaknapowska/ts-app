557fb3716ef3b969d3025d6ebc6c32ee181153df
kasiaknapowska <k.knapowska@gmail.com>
2022-12-01 13:41:11 +0100

    single fetch func, basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,69 +5,98 @@ import { Person } from "./utils/interfaces";
 const App: FC = () => {
   const [people, setPeople] = useState<Person[]>([]);
   const [person, setPerson] = useState<Person | null>(null);
+  const [counter, setCounter] = useState<number>(1);
+  // const fetchData = async (n: number) => {
+  //   const response = await fetch(`https://swapi.dev/api/people/${n}/`);
+  //   const data = await response.json();
+
+  //   const person: Person = {
+  //     name: data.name,
+  //     eye_color: data.eye_color,
+  //     birth_year: data.birth_year,
+  //   };
+  //   setPeople([person]);
+  //   setPerson((prevState) => {
+  //     return {
+  //       ...prevState,
+  //       ...person,
+  //     };
+  //   });
+
+  //   if (!response.ok) {
+  //     const msg = `An error occurred: ${response.status}`;
+  //     throw new Error(msg);
+  //   }
+  // };
+  // const fetchImg = async () => {
+  //   const response = await fetch("https://picsum.photos/534/383");
+  //   const url: string = response.url;
+
+  //   setPerson((prevState) => {
+  //     return {
+  //       ...prevState,
+  //       img: url,
+  //     };
+  //   });
+
+  //   if (!response.ok) {
+  //     const msg = `An error occurred: ${response.status}`;
+  //     throw new Error(msg);
+  //   }
+  // };
 
   const fetchData = async (n: number) => {
     const response = await fetch(`https://swapi.dev/api/people/${n}/`);
     const data = await response.json();
 
+    const responseImg = await fetch("https://picsum.photos/534/383");
+    const imgUrl: string = responseImg.url;
+
+    if (!response.ok || !responseImg.ok) {
+      const msg: string = `An error occurred: ${response.status} || ${responseImg.status}`;
+      throw new Error(msg);
+    }
+
     const person: Person = {
       name: data.name,
       eye_color: data.eye_color,
       birth_year: data.birth_year,
+      img: imgUrl,
     };
-    setPeople([person]);
-    setPerson((prevState) => {
-      return {
-        ...prevState,
-        ...person,
-      };
+    setPerson(person);
+    setPeople((prevState) => {
+      console.log(prevState);
+      return [...prevState, person];
     });
-
-    if (!response.ok) {
-      const msg = `An error occurred: ${response.status}`;
-      throw new Error(msg);
-    }
   };
-  const fetchImg = async () => {
-    const response = await fetch("https://picsum.photos/534/383");
-    const url: string = response.url;
 
-    setPerson((prevState) => {
-      return {
-        ...prevState,
-        img: url,
-      };
-    });
-
-    if (!response.ok) {
-      const msg = `An error occurred: ${response.status}`;
-      throw new Error(msg);
-    }
-  };
   useEffect(() => {
-    fetchData(1);
-    fetchImg();
-  }, []);
+    fetchData(counter);
+  }, [counter]);
 
   useEffect(() => {
     console.log(person);
-  }, [person]);
+    console.log(people);
+  }, [person, people]);
 
   return (
     <div className="App">
-      <section>
-        {person?.img && person?.name ? (
-          <>
+      <section className="container">
+        <p>Kasia Knapowska</p>
+        <button>Formularz rejestracyjny</button>
+        {person ? (
+          <div>
             <img src={person.img} />
             <h1>{person.name}</h1>
             <p>age: {person.birth_year}</p>
             <p>eye color: {person.eye_color}</p>
-          </>
+          </div>
         ) : (
           <h1>Loading...</h1>
         )}
+         <button onClick={() => setCounter(prevState => prevState + 1)}>Next profiles</button>
       </section>
-      <button>Next profiles</button>
+     
     </div>
   );
 };
--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,6 +1,6 @@
 export interface Person {
-    name?: string,
-    eye_color?: string,
-    birth_year?: string,
-    img?: string
+    name: string,
+    eye_color: string,
+    birth_year: string,
+    img: string
   }
